700b6ee6bf7e88da501c13bb5a0f7253a4baab9f
Minha Lee <shjune1994@gmail.com>
2024-04-10 17:30:35 -0400

    Built basic format of Staples price scrapper

--- PriceScrapper/src/scrapper/Main.java
+++ PriceScrapper/src/scrapper/Main.java
@@ -5,8 +5,9 @@ import scrapper.WebDriver;
 
 public class Main {
 	public static void main(String[] args) {
-		// DatabaseConnection db = new DatabaseConnection();
+		DatabaseConnection db = new DatabaseConnection();
 		WebDriver wd = new WebDriver();
+		String result = wd.scrapPrice("https://www.staples.com/24516278/directory_24516278");
 		return;
 	}
 }
--- PriceScrapper/src/scrapper/WebDriver.java
+++ PriceScrapper/src/scrapper/WebDriver.java
@@ -2,6 +2,8 @@ package scrapper;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class WebDriver {
 	private final String WEB_DRIVER_ID = "webdriver.chrome.driver";
@@ -18,9 +20,43 @@ public class WebDriver {
 		options.addArguments("--disable-popup-blocking");
         
 		driver = new ChromeDriver(options);
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String getUrl() {
+		return this.url;
+	}
+	
+	// price by, outofStock by, url, out of stock message
+	public String scrapPrice(String url) {
+		String result = "";
 		
-		url = "https://www.naver.com/";
-		driver.get(url);
-		System.out.println(driver.getPageSource());
+		try {
+			driver.get(url);
+			WebElement curPrice = driver.findElement(By.cssSelector(".price-info__final_price_sku"));
+			WebElement outOfStockSign = driver.findElement(By.xpath("//*[@id='ONE_TIME_PURCHASE']/div/div/div/div/div/div/div[2]/div"));
+			
+			if(curPrice == null) {
+				if(outOfStockSign != null && outOfStockSign.getText().equalsIgnoreCase("This item is out of stock")) {
+					// curPrice
+					// status OUT OF STOCK
+				} else {
+					// curPrice
+					// status ON SITE
+				}
+			} else {
+				// keep RP
+				// status OFF SITE
+			}
+			
+			result = curPrice.getText();
+		} finally {
+			
+		}
+		
+		return result;
 	}
 }
