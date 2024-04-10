131ee7eb11a94b41d0927c3500658e08e8d9d47d
Minha Lee <shjune1994@gmail.com>
2024-04-10 19:00:17 -0400

    Set up the basic multi-threading

--- PriceScrapper/src/scrapper/Main.java
+++ PriceScrapper/src/scrapper/Main.java
@@ -1,13 +1,38 @@
 package scrapper;
 
 import db.DatabaseConnection;
-import scrapper.WebDriver;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.openqa.selenium.By;
+
+import scrapper.WebDriverTask;
 
 public class Main {
 	public static void main(String[] args) {
 		DatabaseConnection db = new DatabaseConnection();
-		WebDriver wd = new WebDriver();
-		String result = wd.scrapPrice("https://www.staples.com/24516278/directory_24516278");
+		
+		ExecutorService executor = Executors.newFixedThreadPool(4);
+		
+		// string, by , by, string object
+		ArrayList<WebDriverTask> tasks = new ArrayList<WebDriverTask>() {{
+			add(new WebDriverTask(
+					"STAPLES", 
+					"https://www.staples.com/24516278/directory_24516278", 
+					new By.ByClassName(".price-info__final_price_sku"), 
+					new By.ByXPath("//*[@id='ONE_TIME_PURCHASE']/div/div/div/div/div/div/div[2]/div"), 
+					"This item is out of stock"));
+		}};
+		
+		for(WebDriverTask task : tasks) {
+			executor.submit(task);
+		}
+		
+		executor.shutdown();
+		
+		//String result = wd.scrapPrice("https://www.staples.com/24516278/directory_24516278");
 		return;
 	}
 }
---
+++ PriceScrapper/src/scrapper/WebDriverTask.java
@@ -0,0 +1,61 @@
+package scrapper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebDriverTask implements Runnable {
+	private final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+	private final String WEB_DRIVER_PATH = "./chromedriver.exe";
+	
+	private String vendor;
+	private String url;
+	private By priceSelector;
+	private By outOfStockSelector;
+	private String outOfStockText;
+	
+	public WebDriverTask(String vendor, String url, By priceSelector, By outOfStockSelector, String outOfStockText) {
+		this.vendor = vendor;
+		this.url = url;
+		this.priceSelector = priceSelector;
+		this.outOfStockSelector = outOfStockSelector;
+		this.outOfStockText = outOfStockText;
+	}
+	
+	private ChromeOptions configChromeOptions() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		options.addArguments("--disable-popup-blocking");
+		
+		return options;
+	}
+
+	@Override
+	public void run() {
+		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+		ChromeOptions options = configChromeOptions();
+		ChromeDriver driver = new ChromeDriver(options);
+		
+		try {
+			driver.get(url);
+			WebElement curPrice = driver.findElement(priceSelector);
+			WebElement outOfStockSign = driver.findElement(outOfStockSelector);
+			
+			if(curPrice == null) {
+				if(outOfStockSign != null && outOfStockSign.getText().equalsIgnoreCase(outOfStockText)) {
+					// curPrice
+					// status OUT OF STOCK
+				} else {
+					// curPrice
+					// status ON SITE
+				}
+			} else {
+				// keep RP
+				// status OFF SITE
+			}
+		} finally {
+			driver.close();
+		}
+	}
+}
